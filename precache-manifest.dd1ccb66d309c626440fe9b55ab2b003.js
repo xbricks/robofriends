59413a65e29e6b864a1fccf8d7130d05b3cbf939
robi saputra <robisaputra@robis-MacBook-Pro.local>
2020-10-06 13:56:23 +0700

    Updates

---
+++ precache-manifest.dd1ccb66d309c626440fe9b55ab2b003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd8366c26c092c48bb785667aa8ff2e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "08e8b2af09cb70f559ea",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d2b2afdeafe1594a3cc6",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "08e8b2af09cb70f559ea",
+    "url": "/robofriends/static/js/2.e09819ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e09819ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b2afdeafe1594a3cc6",
+    "url": "/robofriends/static/js/main.4351ecb5.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
